6c1b1e747f38b15f1bf146da975fe6f9627ce9a2
Boush Li <li.boush@gmail.com>
2016-05-14 01:27:19 +0800

    o Add gitignore file. o Update mass-storage.c.

--- mass-storage.c
+++ mass-storage.c
@@ -18,7 +18,7 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-Email: boush.li@outlook.com
+        Email: boush.li@outlook.com
 ---------------------------------------------------------------------------*/
 
 #include <stdio.h>
